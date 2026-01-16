83fd69bee61dbb1ce1080c41b649ca6922e0dbfb
David Yang <yang200023@gmail.com>
2026-01-16 22:15:33 +1100

    Adding various schemas for admin type

--- app/schemas/users/admin.py
+++ app/schemas/users/admin.py
@@ -1,5 +1,13 @@
 from pydantic import ConfigDict
 from .user import UserBase, UserInDB, UserCreate, UserUpdate
-
 class AdminBase(UserBase):
     model_config = ConfigDict(from_attributes=True)
+
+class AdminCreate(UserCreate):
+    pass
+
+class AdminInDB(UserInDB):
+    pass
+
+class AdminUpdate(UserUpdate):
+    pass
